4db36225e9e56961bd1850ec123c5c789c91c056
Bretin Aymeric <aymeric.bretin@epitech.eu>
2026-02-08 03:29:18 +0100

    feat: server int castle

--- Assets/Scripts/CastleInit.cs
+++ Assets/Scripts/CastleInit.cs
@@ -3,6 +3,24 @@ using UnityEngine;
 
 public class CastleInit : NetworkBehaviour
 {
+    [SerializeField] private int startHealth = 5000;
+    [SerializeField] private int startMaxHealth = 5000;
+
     public NetworkVariable<int> health = new NetworkVariable<int>(5000);
     public NetworkVariable<int> maxHealth = new NetworkVariable<int>(5000);
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            health.Value = startHealth;
+            maxHealth.Value = startMaxHealth;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!IsServer) return;
+        health.Value = Mathf.Max(0, health.Value - damage);
+    }
 }
