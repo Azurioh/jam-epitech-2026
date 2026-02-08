9e975aa413efa49bff77798cb06689096a390192
Cubix <simon.lallement@epitech.eu>
2026-02-08 01:24:11 +0100

    Add player HUD

--- Assets/Scripts/PlayerHUD.cs
+++ Assets/Scripts/PlayerHUD.cs
@@ -1,10 +1,60 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHUD : MonoBehaviour
 {
+    private int playerLayer;
+    private Health healthScript;
+    private PlayerStats statsScript;
+    private CastleInit castleScript;
+    public Image healthBar;
+    public Image castleBar;
+    public Image bugBar;
+    public TextMeshProUGUI moneyText;
+    [SerializeField] private GameObject castle;
+
     void Start()
     {
-        return;
+        playerLayer = LayerMask.NameToLayer("Player");
+    }
+    
+    void Update()
+    {
+        if (!Camera.main) return;
+
+        Transform parent = Camera.main.transform.parent;
+
+        if (!parent || parent.gameObject.layer != playerLayer) return;
+
+        if (!healthScript)
+            healthScript = parent.GetComponent<Health>();
+
+        if (!statsScript)
+            statsScript = parent.GetComponent<PlayerStats>();
+
+        if (!castleScript)
+            castleScript = castle.GetComponent<CastleInit>();
+
+        if (healthScript) {
+            float current = healthScript._currentHealth.Value;
+            float max = healthScript.maxHealth;
+            
+            if (healthBar) healthBar.fillAmount = current / max;
+        }
+
+        if (statsScript) {
+            int goldValue = statsScript.Gold.Value; 
+
+            if (moneyText) moneyText.text = goldValue.ToString();
+        }
+
+        if (castleScript) {
+            float current = castleScript.health;
+            float max = castleScript.maxHealth;
+            
+            if (castleBar) castleBar.fillAmount = current / max;
+        }
     }
 }
